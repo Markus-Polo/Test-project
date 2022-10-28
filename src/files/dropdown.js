b69f1f2cd66e371c9d5b0ecce276b4daf435a899
Mark Polosukhin <markipulooo@gmail.com>
2022-10-29 00:09:02 +0300

    add header

--- src/files/dropdown.js
+++ src/files/dropdown.js
@@ -1,18 +1,6 @@
-import * as flsFunctions from './modules/functions.js'
-
-flsFunctions.isWebp()
-
-/*
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById('dropdown').classList.toggle('show')
 }
-
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches('.dropdown-button')) {
     var dropdowns = document.getElementsByClassName('dropdown-content')
@@ -25,13 +13,9 @@ window.onclick = function (event) {
     }
   }
 }
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function myFunctionTel() {
   document.getElementById('dropdown--tel').classList.toggle('show')
 }
-
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches('.dropdown__button--tel')) {
     var dropdowns = document.getElementsByClassName('.dropdown__content--tel')
