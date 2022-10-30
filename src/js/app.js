c5b2ea81531c8569c3386b1e38713824654c2764
Mark Polosukhin <markipulooo@gmail.com>
2022-10-30 21:24:05 +0200

    add header and hero

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import * as flsFunctions from './modules/functions.js'
+import './modules/dropdown.js'
 
 flsFunctions.isWebp()
 
@@ -6,41 +7,3 @@ flsFunctions.isWebp()
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
 */
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById('dropdown').classList.toggle('show')
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches('.dropdown-button')) {
-    var dropdowns = document.getElementsByClassName('dropdown-content')
-    var i
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i]
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show')
-      }
-    }
-  }
-}
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunctionTel() {
-  document.getElementById('dropdown--tel').classList.toggle('show')
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches('.dropdown__button--tel')) {
-    var dropdowns = document.getElementsByClassName('.dropdown__content--tel')
-    var i
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i]
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show')
-      }
-    }
-  }
-}
